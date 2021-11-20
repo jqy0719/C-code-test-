72c9e5687b6418b5d2c9567c0ef0402348a86b38
jqy0719 <78332867+jqy0719@users.noreply.github.com>
2021-11-20 14:28:13 +0800

    Add files via upload

---
+++ test11_19/test11_19/test.c
@@ -0,0 +1,34 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//#include <string.h>
+//#include <assert.h>
+//
+//void reverse(char* str)
+//{
+//	assert(str);
+//	int len = strlen(str);
+//	char* left = str;
+//	char* right = str + len - 1;
+//
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr[256] = { 0 };
+//	//scanf("%s", arr);
+//	gets(arr);
+//	reverse(arr);
+//
+//	printf("%s\n", arr);
+//	return 0;
+//
+//}
